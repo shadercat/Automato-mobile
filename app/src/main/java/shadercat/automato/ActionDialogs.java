4ecf8d9bdedf517e239408b6de552c0a99318cdb
Thang <shadercatin@gmail.com>
2019-12-23 23:16:33 +0200

    - implemented Action Fragment;

---
+++ app/src/main/java/shadercat/automato/ActionDialogs.java
@@ -0,0 +1,5 @@
+package shadercat.automato;
+
+public class ActionDialogs {
+
+}
--- app/src/main/java/shadercat/automato/ActionFragment.java
+++ app/src/main/java/shadercat/automato/ActionFragment.java
@@ -1,32 +1,27 @@
 package shadercat.automato;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+public class ActionFragment extends Fragment implements View.OnClickListener {
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ActionFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ActionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ActionFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    LinearLayout addMac;
+    LinearLayout delMac;
+    LinearLayout bindMac;
+    LinearLayout unbindMac;
+    LinearLayout delLog;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,45 +29,31 @@ public class ActionFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ActionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ActionFragment newInstance(String param1, String param2) {
-        ActionFragment fragment = new ActionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_action, container, false);
+        View view = inflater.inflate(R.layout.fragment_action, container, false);
+        addMac = view.findViewById(R.id.add_mac_action);
+        delMac = view.findViewById(R.id.delete_mac_action);
+        bindMac = view.findViewById(R.id.bind_mac_action);
+        unbindMac = view.findViewById(R.id.unbind_mac_action);
+        delLog = view.findViewById(R.id.del_log_action);
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onActionFragmentInteraction(uri);
-        }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        addMac.setOnClickListener(this);
+        delMac.setOnClickListener(this);
+        bindMac.setOnClickListener(this);
+        unbindMac.setOnClickListener(this);
+        delLog.setOnClickListener(this);
     }
 
     @Override
@@ -92,18 +73,121 @@ public class ActionFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_mac_action:
+                AddDialog();
+                return;
+            case R.id.delete_mac_action:
+                DelDialog();
+                return;
+            case R.id.bind_mac_action:
+                BindDialog();
+                return;
+            case R.id.unbind_mac_action:
+                UnbindDialog();
+                return;
+            case R.id.del_log_action:
+                DelLogDialog();
+                return;
+        }
+    }
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onActionFragmentInteraction(Uri uri);
+    }
+
+    private void DelDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(getString(R.string.delMac));
+        View inflater = this.getLayoutInflater().inflate(R.layout.dialog_del_alert, null);
+        final EditText macId = inflater.findViewById(R.id.getMacId);
+        final EditText codeMac = inflater.findViewById(R.id.getCode);
+        builder.setView(inflater)
+                .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final String name = macId.getText().toString().trim();
+                        final String code = codeMac.getText().toString().trim();
+                        Toast.makeText(getContext(), name + " " + code, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void AddDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(getString(R.string.addMac));
+        View inflater = this.getLayoutInflater().inflate(R.layout.dialog_del_alert, null);
+        final EditText macId = inflater.findViewById(R.id.getMacId);
+        final EditText codeMac = inflater.findViewById(R.id.getCode);
+        builder.setView(inflater)
+                .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final String name = macId.getText().toString().trim();
+                        final String code = codeMac.getText().toString().trim();
+                        Toast.makeText(getContext(), name + " " + code, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void BindDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(getString(R.string.bindMac));
+        View inflater = this.getLayoutInflater().inflate(R.layout.dialog_del_alert, null);
+        final EditText macId = inflater.findViewById(R.id.getMacId);
+        final EditText codeMac = inflater.findViewById(R.id.getCode);
+        builder.setView(inflater)
+                .setPositiveButton(R.string.bind, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final String name = macId.getText().toString().trim();
+                        final String code = codeMac.getText().toString().trim();
+                        Toast.makeText(getContext(), name + " " + code, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void UnbindDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(getString(R.string.unbindMac));
+        View inflater = this.getLayoutInflater().inflate(R.layout.dialog_del_alert, null);
+        final EditText macId = inflater.findViewById(R.id.getMacId);
+        final EditText codeMac = inflater.findViewById(R.id.getCode);
+        builder.setView(inflater)
+                .setPositiveButton(R.string.unbind, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final String name = macId.getText().toString().trim();
+                        final String code = codeMac.getText().toString().trim();
+                        Toast.makeText(getContext(), name + " " + code, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void DelLogDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(getString(R.string.delLogs));
+        View inflater = this.getLayoutInflater().inflate(R.layout.dialog_del_alert2, null);
+        final EditText macId = inflater.findViewById(R.id.getMacId2);
+        builder.setView(inflater)
+                .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final String name = macId.getText().toString().trim();
+                        Toast.makeText(getContext(), name, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
